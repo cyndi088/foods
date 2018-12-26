80030893e6a7a974043df43351fc208b6aaee9ee
cyndi088 <cyndi088@163.com>
2018-12-26 18:44:25 +0800

    根据其实url获取分类，再根据分类抓取数据

--- foods/spiders/foods12331.py
+++ foods/spiders/foods12331.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-import scrapy
+import re
 import json
+import scrapy
+from scrapy import Request
 from scrapy import FormRequest
 
 
@@ -9,33 +11,53 @@ class Foods12331Spider(scrapy.Spider):
     allowed_domains = ['www.foods12331.cn/web/index.jsp']
     start_urls = ['http://www.foods12331.cn/web/index.jsp']
 
-    def parse(self, response):
-        pass
+    # def parse(self, response):
+    #     url = 'http://www.foods12331.cn/food/detail/findFoodByPage.json'
+    #     res = response.xpath("//div[@class='secenddiv']/a/@onclick").extract()
+    #     # 食品类型
+    #     for food_type in res:
+    #         food_type = self.type_list(food_type)
+    #         # 合格
+    #         qualified_data = '{"food_type": %s , "check_flag": "合格", "order_by": "0", "pageNo": 0, ' \
+    #                          '"pageSize": 20,"bar_code": "", "sampling_province": "", "name_first_letter": null, ' \
+    #                          '"food_name": null}' % food_type
+    #         qualified_data = {
+    #             'filters': qualified_data
+    #         }
+    #         qualified_request = FormRequest(
+    #             url=url, callback=self.qualified_detail, formdata=qualified_data, dont_filter=False
+    #         )
+    #         print('0000000000000000000000000000000000000000000')
+    #         yield qualified_request
 
     def start_requests(self):
         url = 'http://www.foods12331.cn/food/detail/findFoodByPage.json'
+
+        # 合格
         qualified_data = {
-            'filters': '{"food_type": "粮食加工品", "check_flag": "合格", "order_by": "0", "pageNo": 473, "pageSize": 200, '
+            'filters': '{"food_type": "粮食加工品" , "check_flag": "合格", "order_by": "0", "pageNo": 0, "pageSize": 20, '
                        '"bar_code": "", "sampling_province": "", "name_first_letter": null, "food_name": null}'
         }
+        print(qualified_data)
+        qualified_request = FormRequest(
+            url=url, formdata=qualified_data, callback=self.qualified_detail, dont_filter=False
+        )
+
+        # 不合格
         unqualified_data = {
             'filters': '{"food_type": "粮食加工品", "check_flag": "不合格", "order_by": "0", "pageNo": 0, "pageSize": 20, '
                        '"bar_code": "", "sampling_province": "", "name_first_letter": null, "food_name": null}'
         }
-        qualified_request = FormRequest(
-            url=url, formdata=qualified_data, callback=self.qualified_detail, dont_filter=True
-        )
         unqualified_request = FormRequest(
-            url=url, formdata=unqualified_data, callback=self.unqualified_detail, dont_filter=True
+            url=url, formdata=unqualified_data, callback=self.unqualified_detail, dont_filter=False
         )
 
         yield qualified_request
 
-
     def qualified_detail(self, response):
         res = json.loads(response.text,  encoding='utf-8')
         print('111111111111111111111111111111111111111')
-        print(response.text)
+        print(res)
         print('111111111111111111111111111111111111111')
 
     def unqualified_detail(self, response):
@@ -43,3 +65,9 @@ class Foods12331Spider(scrapy.Spider):
         print('222222222222222222222222222222222222222')
         print(response.text)
         print('222222222222222222222222222222222222222')
+
+    @staticmethod
+    def type_list(str):
+        str_list = str.split("'")
+        res = str_list[1]
+        return res
